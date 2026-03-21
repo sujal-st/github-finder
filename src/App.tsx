e2785bed72189cc5dbf47bf1d29e719c0f74fc58
sujal-st <sujal.sthapit4@gmail.com>
2026-03-21 15:37:09 +0545

    feat(search-user)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import UserSearch from "./components/userSearch";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>GitHub Finder</h1>
+      <UserSearch/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/UserSearch.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import { useQuery } from "@tanstack/react-query"
+import { FaGithubAlt } from "react-icons/fa";
+
+
+function UserSearch() {
+    const [username, setUsername]= useState("");
+    const [submittedUsername, setSubmittedUsername]= useState("");
+
+    const {data, isLoading, isError, error}= useQuery({
+        queryKey: ['users', submittedUsername],
+        queryFn: async ()=>{
+            const res = await fetch(`${import.meta.env.VITE_GITHUB_API_URL}/users/${submittedUsername}`);
+
+            if(!res.ok) throw new Error("User not found");
+
+            const data= await res.json();
+            console.log(data);
+            return data;
+        },
+        enabled: !!submittedUsername, // !! turns the submitedUsername into boolean and if empty string then it will be false else true
+    });
+
+    const handleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        setSubmittedUsername(username.trim());
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit} className="form">
+                <input type="text" placeholder="Enter GitHub username..." value={username} onChange={(e)=>setUsername(e.target.value)} />
+                <button type="submit">Search</button>
+                {isLoading && <p className="status">
+                    Loading...</p>}
+                {isError && <p className="status error">
+                    {error.message}</p>}
+
+                {data && (
+                    <div className="user-card">
+                        <img src={data.avatar_url} alt={data.name} className="avatar"/>
+                        <h2>{data.name || data.login}</h2>
+                        <p className="bio">{data.bio}</p>
+                        <a href={data.html_url} className="profile-btn" target="_blank" rel="noopener noreferrer">
+                            <FaGithubAlt/> View GitHub Profile
+                        </a>
+                    </div>
+                )}
+            </form>
+        </>
+    )
+}
+
+export default UserSearch
